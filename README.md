db0040c02f7d99ef4ede430069aa816f882e0daf
Guilherme Bittencourt de Borba <gui.bittborba@gmail.com>
2022-01-04 15:20:48 -0300

    adds metadata syntaxis parsing and file metadata editing using eyeD3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # youtube_dl_helper
 
-A helper program meant to automate the process of downloading mp3 files from YouTube using youtube-dl library
+A helper program meant to automate the process of downloading mp3 files from YouTube using youtube-dl library, as well as setting the file's metadata according to a self-made syntaxis from a source "track list" text file.
 
 ## Dependencies
 
@@ -9,6 +9,7 @@ In order to run _youtube_dl_helper_, you will need (apart from python, obviously
 - selenium
 - youtube-dl
 - ffmpeg
+- eyeD3
 
 All of these dependencies can be acquired for free from the web, by using services like ```pip```, ```brew``` or ```choco```
 
@@ -17,9 +18,9 @@ All of these dependencies can be acquired for free from the web, by using servic
 1. Create a text file containing desired track titles, one per line (// will denote the line as a comment, and it will be ignored!)
 2. Run ```python main.py``` and pass in your ```.txt``` file path
 3. Wait for the automated process to unfold...
-4. Enjoy yout freshly downloaded mp3 files!
+4. Enjoy your freshly downloaded mp3 files!
 
 
 ### Note
 
-An example track list text file can be found on this very repository ( _example_track_list.txt_ )
+An example track list text file (with all is syntactic flairs for metadata parsing) can be found on this very repository ( _example_track_list.txt_ )
--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.remote_connection import LOGGER, logging
 
 def main():
     YOUTUBE_URL = "http://www.youtube.com"
     source_file = None
     video_titles = []
     video_links = []
+    track_metadata = []
 
     print(">> Welcome to youtube_dl_helper!")
 
@@ -27,16 +29,62 @@ def main():
 
     print(">> Parsing " + source_file_path + "...")
 
-    for title in source_file: # Reads textfile line by line
-        if title != '\n' and '//' not in title:
-            video_titles.append(title[:-1]) # Stores content of each line, minus \n character
-                                            # Lines that contain a // are considered a comment, and are ignored
+    for line in source_file: # Reads textfile line by line
+        if line != '\n' and '//' not in line: # we will be parsing this line (its neither an empty line nor a comment)
+
+            if '||' not in line: # this is a simple no-metadata line that only contains search name / file name parameter
+                video_titles.append(line[:-1])  # Stores content of each line, minus \n character
+                                                # Lines that contain a // are considered a comment, and are ignored
+                track_metadata.append([None, None, None]) # Store all Nones for empty metadata
+                continue
+
+            # at this point, we know we are messing with some metadata
+            # now lets check for that convenient - separator syntax
+            res = '-' in line
+            if res:
+                # we have it!
+                res = line.index('-')
+                if res >= 0:
+                    # no '-' in artist title allowed, they wil always split artist title and track title
+                    track_metadata.append([line[:res - 1], line[line.find("||") + 3:-1], line[res + 2: line.find("||") - 1]])
+                    # grab the search name and store it
+                    video_titles.append(line[:line.find('||') - 1])
+                    continue
+
+            # we don't have it! we'll do it the hard way then
+            metadata = [None, None, None]
+
+            # first, grab the search name and store it
+            video_titles.append(line[:line.find('||') - 1])
+
+            # now fill the rest of the metadata
+            line = line[line.find('||') + 3:]
+            artist = line[:line.index('||') - 1]
+            if artist != "NULL":
+                metadata[0] = artist
+
+            line = line[line.index('||') + 3:]
+            album = line[:line.index('||') - 1]
+            if album != "NULL":
+                metadata[1] = album
+
+            line = line[line.index('||') + 3:]
+            track_title = line[:-1]
+            if track_title != "NULL":
+                metadata[2] = track_title
+
+            # punch it in
+            track_metadata.append(metadata)
 
     source_file.close() # Close source file; We won't be needing it anymore
 
     track_count = len(video_titles)
 
-    print(">> " + str(track_count) + " track titles parsed from " + source_file_path)
+    print(">> Done! [" + str(track_count) + "] tracks parsed from " + source_file_path)
+
+    print(">> Starting Chrome WebDriver...")
+
+    LOGGER.setLevel(logging.WARNING) # Reduces verbosity of ChromeWebDriver
 
     driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
     driver.maximize_window()
@@ -49,6 +97,17 @@ def main():
 
     current_track_number = 1
     for title in video_titles:
+        # before trying to download using title, we need to clean it up a bit
+        # by escaping any ' that are present with a \
+        res = "'" in title
+        while res:
+            res = False
+            for i, char in enumerate(title):
+                if char == "'" and title[i - 1] != "\\":
+                    title = title[:i] + "\\" + title[i:]
+                    res = True
+                    break
+
         print(">> Storing URI [" + str(current_track_number) + " of " + str(track_count) + "]...")
 
         driver.get(YOUTUBE_URL + "/results?search_query=" + title)
@@ -69,16 +128,51 @@ def main():
 
     print(">> URIs ready! Starting downloads...")
 
-    current_track_number = 1
+    current_track_number = 0
     for title, link in zip(video_titles, video_links):
-        print(">> Downloading track [" + str(current_track_number) + " of " + str(track_count) + "]...")
-
-        try:
-            os.system('youtube-dl -o "audio_files/' + title + '.%(ext)s" --rm-cache-dir --extract-audio --audio-format mp3 ' + link)
-        except:
-            pass
-
         current_track_number += 1
+        print(">> Downloading track [" + str(current_track_number) + " of " + str(track_count) + "]...")
+        try:
+            # --rm-cache-dir flag prevents weird 403 HTTP errors from happening
+            res = os.system('youtube-dl -o "audio_files/' + title + '.%(ext)s" --rm-cache-dir --extract-audio --audio-format mp3 ' + link)
+        except Exception as e:
+            print(e)
+            # // TODO: maybe do some sort of recovery here? (loop back and try again a couple of times? maybe loop back at the end of the tracklist?)
+            print(">> ERROR downloading track [" + str(current_track_number) + " of " + str(track_count) + "] !!! Skipping to next track...")
+            continue
+
+        if res:
+            # res == 1 means an error during download operation
+            # // TODO: maybe do some sort of recovery here? (loop back and try again a couple of times? maybe loop back at the end of the tracklist?)
+            print(">> ERROR downloading track [" + str(current_track_number) + " of " + str(track_count) + "] !!! Skipping to next track...")
+            continue
+
+        # res == 0 means a successfull download operation
+        # Since all went well for this track, all that is left to do
+        # is set this file's metadata (if any is specified)
+        current_track_metadata = track_metadata[current_track_number - 1]
+        if current_track_metadata == [None, None, None]:
+            # no metadata, move on to the next track
+            continue
+
+        print(">> Applying file metadata to track [" + str(current_track_number) + " of " + str(track_count) + "]...")
+        # at least some metadata; grab it
+        artist_metadata = current_track_metadata[0]
+        album_title_metadata = current_track_metadata[1]
+        track_title_metadata = current_track_metadata[2]
+
+        # now apply it
+        metadata_command =  'eyeD3 -Q' # -Q arg turns on "quiet" mode (less verbose output)
+        if artist_metadata != None:
+            metadata_command +=  ' -a "' + artist_metadata + '" --text-frame=TPE2:"' + artist_metadata + '"'
+        if album_title_metadata != None:
+            metadata_command += ' -A "' + album_title_metadata + '"'
+        if track_title_metadata != None:
+            metadata_command += ' -t "' + track_title_metadata + '"'
+
+        metadata_command += ' "audio_files/' + title + '.mp3"'
+
+        res = os.system(metadata_command)
 
     print(">> Process Complete!")
 
